3fb3ccf061776c349e46eea8bdef6d8ba6acb072
zhaoxh <1418094594@qq.com>
2023-10-11 10:57:27 +0800

    Update XWXPayEntryActivity.java
    替换Nullable的引用

--- android/src/main/java/com/puti/paylib/XWXPayEntryActivity.java
+++ android/src/main/java/com/puti/paylib/XWXPayEntryActivity.java
@@ -3,7 +3,8 @@ package com.puti.paylib;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+// import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
