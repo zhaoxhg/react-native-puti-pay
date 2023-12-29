43da675971004a925b14d6e4cfe98e735c52baa6
zhaoxh <1418094594@qq.com>
2023-12-29 16:32:55 +0800

    Update index.js
    微信支付添加跳转小程序支付功能

--- index.js
+++ index.js
@@ -70,4 +70,23 @@ export default class XPay {
     static wxPay(params, callBack) {
         NativeModules.PutiPay.wxPay(params, callBack)
     }
+
+    /**
+     * 微信跳转小程序支付
+     * 传入参数示例
+     * {
+        userName:data.userName, //填小程序原始id
+        path: data.path,//拉起小程序页面的可带参路径，不填默认拉起小程序首页，对于小游戏，可以只传入 query 部分，来实现传参效果，如：传入 "?foo=bar"。
+        miniprogramType: data.miniprogramType,// 正式版=0| 开发版=1 | 体验版=2
+       }
+     *
+     *
+     * @param params  参数
+     * @param callBack 回调结果码 0:支付成功,
+     *                          -1:原因：支付错误,可能的原因：签名错误、未注册APPID、项目设置APPID不正确、注册的APPID与设置的不匹配、其他异常等
+     *                          -2: 原因 用户取消,无需处理。发生场景：用户不支付了，点击取消，返回APP
+     */
+    static wxMiniPay(params, callBack) {
+        NativeModules.PutiPay.wxMiniPay({miniprogramType:0, ...params}, callBack)
+    }
 }
